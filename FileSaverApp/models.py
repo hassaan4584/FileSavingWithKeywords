15ef80506a5392162cfacefe74f16395caec8112
Hassaan Fayyaz <hassaan.fayyaz@gmail.com>
2017-12-25 11:53:16 +0500

    file being saved in its respective category folder

--- FileSaverApp/models.py
+++ FileSaverApp/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
 # Create your models here.
+def get_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/pdfs/<document_type>/<filename>
+    return 'pdfs/{0}/{1}'.format(instance.document_type, filename)
 
 class Document(models.Model):
     document_name = models.CharField(max_length=200)
     document_type = models.CharField(max_length=100, default='General')
-    file = models.FileField(upload_to="pdfs/")
+    file = models.FileField(upload_to=get_directory_path)
     document_text = models.CharField(max_length=2000, default='')
 
     def __str__(self):
-        return self.file_name
+        return self.document_name
+
 
 
 class Keyword(models.Model):
